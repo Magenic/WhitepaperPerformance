2db9fc85502c5c9f662fe36f9303b415bcf6b20c
unknown <KevinF@KevinF-PC3.Magenic.net>
2015-07-30 13:17:24 -0400

    #28 Start of add incident view model.

--- Xamarin/Xamarin.IncidentApp/Xamarin.IncidentApp/ViewModels/AddIncidentViewModel.cs
+++ Xamarin/Xamarin.IncidentApp/Xamarin.IncidentApp/ViewModels/AddIncidentViewModel.cs
@@ -5,8 +5,54 @@ namespace Xamarin.IncidentApp.ViewModels
 {
     public class AddIncidentViewModel : BaseViewModel
     {
-        public AddIncidentViewModel(INetworkService networkService, IUserDialogs userDialogs) : base(networkService, userDialogs)
+        public AddIncidentViewModel(INetworkService networkService, IUserDialogs userDialogs)
+            : base(networkService, userDialogs)
         {
+
+        }
+
+        private string _subject;
+        public string Subject
+        {
+            get { return _subject; }
+            set
+            {
+                _subject = value;
+                RaisePropertyChanged(() => Subject);
+            }
+        }
+
+        private string _description;
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+                RaisePropertyChanged(() => Description);
+            }
+        }
+
+        private string _description;
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+                RaisePropertyChanged(() => Description);
+            }
+        }
+
+        private byte[] _image;
+        public byte[] Image
+        {
+            get { return _image; }
+            set
+            {
+                _image = value;
+                RaisePropertyChanged(() => Image);
+            }
         }
     }
 }
