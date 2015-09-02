a11ca9fbecb3fea0f363ef9c8b5e40490cd444ad
Bradley Dean <Bradley@TrueGeek.com>
2015-09-02 15:57:53 -0400

    PhoneGap - changed way incident queue list loads

--- PhoneGap/www/js/controllers/incidentscontroller.js
+++ PhoneGap/www/js/controllers/incidentscontroller.js
@@ -7,13 +7,27 @@ app.controller('IncidentsController', function($scope, Azure, $stateParams, $sta
   $scope.selectedUserFullName = userFullName;
 
   // setup open / closed
-  $scope.showOpen = true;
+  $scope.currentlyShowingOpen = true;
 
   // do initial load of incidents
   refreshIncidents();
 
   $scope.doRefresh = function() {
     refreshIncidents();
+  };
+
+  $scope.showOpen = function() {
+    if ($scope.currentlyShowingOpen != true) {
+        $scope.currentlyShowingOpen = true;
+        refreshIncidents();
+    }
+  };
+
+  $scope.showClosed = function() {
+    if ($scope.currentlyShowingOpen != false) {
+        $scope.currentlyShowingOpen = false;
+        refreshIncidents();
+    }
   };
 
   $scope.selectIncident = function(incidentId) {
@@ -29,7 +43,8 @@ app.controller('IncidentsController', function($scope, Azure, $stateParams, $sta
     Azure.getIncidentList(userId)
       .then(function (incidentList) {
 
-        $scope.incidentList = incidentList;
+        var showClosed = !$scope.currentlyShowingOpen;
+        $scope.incidentList = incidentList.filter(function (el) { return el.closed == showClosed; });
         $scope.$broadcast('scroll.refreshComplete');
 
       });
